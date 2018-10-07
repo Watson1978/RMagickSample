2a0348d89fbd1e8d55e13961e446eb2a051a1faf
Watson <watson1978@gmail.com>
2018-10-07 23:27:14 +0900

    composite_tiled

---
+++ composite_tiled.rb
@@ -0,0 +1,19 @@
+require 'rmagick'
+
+# Create a transparent image to tile over the background image.
+wm = Magick::Image.read("xc:none") { self.size = "100x50" }.first
+
+# Draw "RMagick" in semi-transparent text on the transparent image.
+gc = Magick::Draw.new
+gc.fill '#ff0000'
+gc.font_weight Magick::BoldWeight
+gc.font_size 18
+gc.rotate 15
+gc.gravity Magick::CenterGravity
+gc.text 0, 0, "RMagick"
+gc.draw wm
+
+# Read the background image.
+image = Magick::ImageList.new('./Flower_Hat.jpg').first
+
+image.composite_tiled(wm).write('./result/composite_tiled.png')
