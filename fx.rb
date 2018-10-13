34f845fac61f5ba189d02e21612726cb5b796c56
Watson <watson1978@gmail.com>
2018-10-13 22:16:37 +0900

    fx

---
+++ fx.rb
@@ -0,0 +1,4 @@
+require 'rmagick'
+
+image = Magick::ImageList.new('./Flower_Hat.jpg')
+image.fx('xx=i-w/2; yy=j-h/2; rr=hypot(xx,yy); (.5-rr/140)*1.2+.5', Magick::BlueChannel).write('./result/fx.png')
