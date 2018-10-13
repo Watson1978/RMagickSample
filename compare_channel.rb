1631dadcb2594c0991b82dd0ef9d1e70dbd1b044
Watson <watson1978@gmail.com>
2018-10-13 17:07:25 +0900

    compare_channel

---
+++ compare_channel.rb
@@ -0,0 +1,5 @@
+require 'rmagick'
+
+image = Magick::ImageList.new('./Flower_Hat.jpg').first
+image2 = image.channel(Magick::BlueChannel)
+image.compare_channel(image2, Magick::MeanSquaredErrorMetric)[0].write('./result/compare_channel.png')
