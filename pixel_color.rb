88f374c1392b23a9076bcc2308188fd50bcb5a14
Watson <watson1978@gmail.com>
2018-10-28 15:30:06 +0900

    pixel_color

---
+++ pixel_color.rb
@@ -0,0 +1,6 @@
+require 'rmagick'
+
+image = Magick::ImageList.new('./Flower_Hat.jpg').first
+p image.pixel_color(150, 150).to_color
+p image.pixel_color(150, 150, 'red').to_color
+p image.pixel_color(150, 150).to_color
