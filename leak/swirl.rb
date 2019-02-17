7a6bffeebe3c1c8d7a08bd8cb0f4af09d23a79ad
Watson <watson1978@gmail.com>
2019-02-17 17:49:55 +0900

    add leak tests

---
+++ leak/swirl.rb
@@ -0,0 +1,16 @@
+require 'rmagick'
+
+image = Magick::Image.new(20, 20)
+
+200000.times do |i|
+  begin
+    image.swirl('x')
+  rescue
+  end
+
+  GC.start if i % 100 == 0
+end
+
+GC.start
+rss = `ps -o rss= -p #{Process.pid}`.to_i / 1024
+puts "Process: #{Process.pid}: RSS = #{rss} MB"
