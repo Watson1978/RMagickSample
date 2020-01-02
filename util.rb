16081f59ecbe57de1b115efb1ea273ee2362afb8
Watson <watson1978@gmail.com>
2020-01-02 19:31:28 +0900

    update

--- util.rb
+++ util.rb
@@ -1,6 +1,15 @@
 require 'rmagick'
 require 'fileutils'
 
+class Magick::ImageList
+  alias_method :original_write, :write
+  def write(file)
+    version = Magick::Magick_version.split[1]
+    FileUtils.mkdir_p("result-#{version}")
+    self.original_write(File.join("result-#{version}", file))
+  end
+end
+
 class Magick::Image
   alias_method :original_write, :write
   def write(file)
@@ -9,3 +18,12 @@ class Magick::Image
     self.original_write(File.join("result-#{version}", file))
   end
 end
+
+
+
+
+
+
+
+
+
